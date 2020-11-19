b5b453eb7aad88518d77f7e45b077911200083d7
RobBennettUK <38293815+RobBennettUK@users.noreply.github.com>
2020-11-19 18:36:46 +0000

    Lesson Completed

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function Note(props) {
   function handleClick() {
@@ -9,9 +10,13 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
 
 export default Note;
+
+// The benefit of adding react components, means we can add components that have functionality already built in!
